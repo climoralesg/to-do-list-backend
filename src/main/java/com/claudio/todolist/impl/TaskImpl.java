e0ef182ab02f081465f23a63dab9ed93dd730dab
Claudio Morales <climoralesg@gmail.com>
2025-06-11 00:27:34 -0400

    develop: delete query dependency

--- src/main/java/com/claudio/todolist/impl/TaskImpl.java
+++ src/main/java/com/claudio/todolist/impl/TaskImpl.java
@@ -3,7 +3,6 @@ package com.claudio.todolist.impl;
 import java.util.HashMap;
 import java.util.List;
 
-//import javax.management.Query;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
